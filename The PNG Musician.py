681f6ec693f5dd9eb32fe2820254904820c4456c
Sagar Dev Achar <51400137+SagarDevAchar@users.noreply.github.com>
2022-08-07 08:53:02 +0530

    Add files via upload

--- The PNG Musician.py
+++ The PNG Musician.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 import numpy as np
 from os import listdir
@@ -37,88 +39,90 @@ def print_ascii_image(img: np.array, norm=False):
 
 
 if __name__ == '__main__':
-    SECTION_SEPERATOR = '\n' + '-' * 200 + '\n'
+    while True:
+        SECTION_SEPERATOR = '\n' + '-' * 200 + '\n'
 
-    LOGO = """
-                                        ████████╗██╗  ██╗███████╗    ██████╗ ███╗   ██╗ ██████╗     ███╗   ███╗██╗   ██╗███████╗██╗ ██████╗██╗ █████╗ ███╗   ██╗
-                                        ╚══██╔══╝██║  ██║██╔════╝    ██╔══██╗████╗  ██║██╔════╝     ████╗ ████║██║   ██║██╔════╝██║██╔════╝██║██╔══██╗████╗  ██║
-                                           ██║   ███████║█████╗      ██████╔╝██╔██╗ ██║██║  ███╗    ██╔████╔██║██║   ██║███████╗██║██║     ██║███████║██╔██╗ ██║
-                                           ██║   ██╔══██║██╔══╝      ██╔═══╝ ██║╚██╗██║██║   ██║    ██║╚██╔╝██║██║   ██║╚════██║██║██║     ██║██╔══██║██║╚██╗██║
-                                           ██║   ██║  ██║███████╗    ██║     ██║ ╚████║╚██████╔╝    ██║ ╚═╝ ██║╚██████╔╝███████║██║╚██████╗██║██║  ██║██║ ╚████║
-                                           ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚═╝     ╚═╝  ╚═══╝ ╚═════╝     ╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝ ╚═════╝╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
-                                                                                                                                                                
-                                                                 █▀▄ █ █   █▀▀ █▀█ █▀▀ █▀█ █▀▄   █▀▄ █▀▀ █ █   █▀█ █▀▀ █ █ █▀█ █▀▄
-                                                                 █▀▄  █    ▀▀█ █▀█ █ █ █▀█ █▀▄   █ █ █▀▀ ▀▄▀   █▀█ █   █▀█ █▀█ █▀▄
-                                                                 ▀▀   ▀    ▀▀▀ ▀ ▀ ▀▀▀ ▀ ▀ ▀ ▀   ▀▀  ▀▀▀  ▀    ▀ ▀ ▀▀▀ ▀ ▀ ▀ ▀ ▀ ▀
-    """ + SECTION_SEPERATOR
+        LOGO = """
+                                            ████████╗██╗  ██╗███████╗    ██████╗ ███╗   ██╗ ██████╗     ███╗   ███╗██╗   ██╗███████╗██╗ ██████╗██╗ █████╗ ███╗   ██╗
+                                            ╚══██╔══╝██║  ██║██╔════╝    ██╔══██╗████╗  ██║██╔════╝     ████╗ ████║██║   ██║██╔════╝██║██╔════╝██║██╔══██╗████╗  ██║
+                                               ██║   ███████║█████╗      ██████╔╝██╔██╗ ██║██║  ███╗    ██╔████╔██║██║   ██║███████╗██║██║     ██║███████║██╔██╗ ██║
+                                               ██║   ██╔══██║██╔══╝      ██╔═══╝ ██║╚██╗██║██║   ██║    ██║╚██╔╝██║██║   ██║╚════██║██║██║     ██║██╔══██║██║╚██╗██║
+                                               ██║   ██║  ██║███████╗    ██║     ██║ ╚████║╚██████╔╝    ██║ ╚═╝ ██║╚██████╔╝███████║██║╚██████╗██║██║  ██║██║ ╚████║
+                                               ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚═╝     ╚═╝  ╚═══╝ ╚═════╝     ╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝ ╚═════╝╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+                                                                                                                                                                    
+                                                                     █▀▄ █ █   █▀▀ █▀█ █▀▀ █▀█ █▀▄   █▀▄ █▀▀ █ █   █▀█ █▀▀ █ █ █▀█ █▀▄
+                                                                     █▀▄  █    ▀▀█ █▀█ █ █ █▀█ █▀▄   █ █ █▀▀ ▀▄▀   █▀█ █   █▀█ █▀█ █▀▄
+                                                                     ▀▀   ▀    ▀▀▀ ▀ ▀ ▀▀▀ ▀ ▀ ▀ ▀   ▀▀  ▀▀▀  ▀    ▀ ▀ ▀▀▀ ▀ ▀ ▀ ▀ ▀ ▀
+        """ + SECTION_SEPERATOR
 
-    print(LOGO)
+        print(LOGO)
 
-    print("Hey There!\nI'm THE PNG MUSICIAN!")
-    print("I convert PNG Images to MP3 Audio and play them!")
-    print("\nWell that's not exactly how I work, but it's one way to put it :P")
-    print("\nAnyways, maximize this window for the best experience ;)")
-
-    print(SECTION_SEPERATOR)
-
-    try:
-        file_list = listdir('SRC')
-        png_file_list = []
-        for filename in file_list:
-            if filename[-4:] == '.png':
-                png_file_list.append(filename)
-
-        print("I found %d PNG files!\nWhich one do you want me to play?" % len(png_file_list))
-        for png_file in png_file_list:
-            print(f'\t{png_file_list.index(png_file)+1:2d}) {png_file:s}')
-
-        file_index = -1
-        while True:
-            try:
-                file_index = int(input('\nEnter an image number: ').strip())
-                if 0 < file_index <= len(png_file_list):
-                    file_index -= 1
-                    break
-                else:
-                    raise ValueError
-            except Exception:
-                print("Hmmm, that's not quite what I expected. How about you give it another try!")
-        SONG_NAME = png_file_list[file_index]
+        print("Hey There!\nI'm THE PNG MUSICIAN!")
+        print("I convert PNG Images to MP3 Audio and play them!")
+        print("\nWell that's not exactly how I work, but it's one way to put it :P")
+        print("\nAnyways, maximize this window for the best experience ;)")
 
         print(SECTION_SEPERATOR)
 
-        print("Reading Image...")
-        STEGANO_IMAGE = cv2.imread(fr'SRC\{SONG_NAME:s}', cv2.IMREAD_COLOR)
-        H, W, D = STEGANO_IMAGE.shape
+        try:
+            file_list = listdir('SRC')
+            png_file_list = []
+            for filename in file_list:
+                if filename[-4:] == '.png':
+                    png_file_list.append(filename)
 
-        print("Creating Music from the Image...")
-        MP3_DATA = parse_stegano_or(STEGANO_IMAGE & 0x3)
+            print("I found %d PNG files!\nWhich one do you want me to play?" % len(png_file_list))
+            for png_file in png_file_list:
+                print(f'\t{png_file_list.index(png_file)+1:2d}) {png_file:s}')
 
-        print("Writing Music to out.mp3...")
-        with open('out.mp3', 'wb') as mp3_file:
-            mp3_file.write(MP3_DATA)
+            file_index = -1
+            while True:
+                try:
+                    file_index = int(input('\nEnter an image number: ').strip())
+                    if 0 < file_index <= len(png_file_list):
+                        file_index -= 1
+                        break
+                    else:
+                        raise ValueError
+                except Exception:
+                    print("Hmmm, that's not quite what I expected. How about you give it another try!")
+            SONG_NAME = png_file_list[file_index]
+
+            print(SECTION_SEPERATOR)
+
+            print("Reading Image...")
+            STEGANO_IMAGE = cv2.imread(fr'SRC\{SONG_NAME:s}', cv2.IMREAD_COLOR)
+            H, W, D = STEGANO_IMAGE.shape
+
+            print("Creating Music from the Image...")
+            MP3_DATA = parse_stegano_or(STEGANO_IMAGE & 0x3)
+
+            print("Writing Music to out.mp3...")
+            with open('out.mp3', 'wb') as mp3_file:
+                mp3_file.write(MP3_DATA)
+
+            print(SECTION_SEPERATOR)
+
+            print("{:^200s}".format(f"Currently Playing {SONG_NAME}\n"))
+            ascii_image_w = (len(SECTION_SEPERATOR) - 6) // 2
+            ascii_image_h = ascii_image_w * 9 // 16
+            print_ascii_image(cv2.cvtColor(cv2.resize(STEGANO_IMAGE, (ascii_image_w, ascii_image_h)), cv2.COLOR_BGR2GRAY))
+            print('')
+
+            player_subprocess = subprocess.Popen(['bin/mpg123', '-q', 'out.mp3'])
+
+            print("{:^200s}".format('Press ENTER to stop playing the song'), end='')
+            while player_subprocess.poll() is None:
+                if msvcrt.kbhit():
+                    if msvcrt.getch() == b'\r':
+                        break
+            print("\r{:^200s}".format('Finished Playing!'))
+
+            player_subprocess.kill()
+
+        except FileNotFoundError:
+            print("Looks like you have lost the 'bin' or 'SRC' folder!\nUnfortunately, I cannot work without those...")
 
         print(SECTION_SEPERATOR)
-
-        print("{:^200s}".format(f"Currently Playing {SONG_NAME}\n"))
-        ascii_image_w = (len(SECTION_SEPERATOR) - 6) // 2
-        ascii_image_h = ascii_image_w * 9 // 16
-        print_ascii_image(cv2.cvtColor(cv2.resize(STEGANO_IMAGE, (ascii_image_w, ascii_image_h)), cv2.COLOR_BGR2GRAY))
-        print('')
-
-        player_subprocess = subprocess.Popen(['bin/mpg123', '-q', 'out.mp3'])
-
-        print("{:^200s}".format('Press ENTER to stop playing the song'), end='')
-        while player_subprocess.poll() is None:
-            if msvcrt.kbhit():
-                if msvcrt.getch() == b'\r':
-                    break
-        print("\r{:^200s}".format('Finished Playing!'))
-
-        player_subprocess.kill()
-
-    except FileNotFoundError:
-        print("Looks like you have lost the 'bin' or 'SRC' folder!\nUnfortunately, I cannot work without those...")
-
-    print(SECTION_SEPERATOR)
-    input("Press ENTER to release my process into the void")
+        input("Press ENTER to start over")
+        os.system('cls')
